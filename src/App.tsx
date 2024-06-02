e6adbc5a8bd73f9e107263ec61682e6809b7d3af
ing.jarillon <ing.jarillon@gmail.com>
2024-06-02 21:35:45 +0000

    add perfectionist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-  <h1 className="text-3xl font-bold underline text-violet-500">
-    Matriz de suelo
-  </h1>
+      <h1 className="text-3xl font-bold underline text-violet-500">
+        Matriz de suelo
+      </h1>
     </>
   )
 }
