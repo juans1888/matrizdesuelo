11eb43c4ed9bb1e09cf30d2595a4d04fe7132f09
ing.jarillon <ing.jarillon@gmail.com>
2024-06-03 17:03:37 +0000

    add d3

--- src/pages/piles/index.tsx
+++ src/pages/piles/index.tsx
@@ -1,5 +1,34 @@
+import * as d3 from 'd3'
+
+import { useEffect, useRef } from 'react'
+
+const data = [1, 2]
+
 function Piles() {
-  return <div>Pilotes</div>
+  const chartRef = useRef(null)
+
+  useEffect(() => {
+    var svg = d3
+      .select(chartRef.current)
+      .append('svg')
+      .attr('width', 300)
+      .attr('height', 200)
+
+    svg
+      .append('rect')
+      .attr('x', 10)
+      .attr('y', 120)
+      .attr('width', 200)
+      .attr('height', 40)
+      .attr('stroke', 'black')
+      .attr('fill', '#69a3b2')
+
+    return () => {
+      svg.remove()
+    }
+  }, [data])
+
+  return <div ref={chartRef}></div>
 }
 
 export default Piles
